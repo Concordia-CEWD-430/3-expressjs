58d05fd5346bd56ab09e73e47a1170853b8d2061
PranavBhatia <bhatiapranav1996@gmail.com>
2024-01-29 17:37:14 -0500

    Parsing incoming req

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded());
+
 app.use("/", (req, res, next) => {
-  console.log("Common Middleware");
   next();
 });
 
-app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Add Product Page</h1>");
+app.use("/add", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Book</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("Generic route");
   res.send("<h1>Hello</h1>");
 });
 
