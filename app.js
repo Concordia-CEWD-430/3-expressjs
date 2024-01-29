84677431e05ac15a36efa8b4de231568e36daa67
PranavBhatia <bhatiapranav1996@gmail.com>
2024-01-29 16:53:45 -0500

    Return html

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("another express js middleware");
+  res.send("<h1>Hello</h1>");
 });
 
 const server = http.createServer(app);
