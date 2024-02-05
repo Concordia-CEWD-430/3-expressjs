91dedc8e358f0e111f8095aa0501a7a5dd6a5eca
PranavBhatia <bhatiapranav1996@gmail.com>
2024-02-05 17:35:58 -0500

    Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/add-book", (req, res, next) => {
-  res.send(
-    "<form action='/book' method='POST'><input type='text' name='title'><button type='submit'>Add Book</button></form>"
-  );
+  res.send("<form action='/admin/add-book' method='POST'><input type='text' name='title'><button type='submit'>Add Book</button></form>");
 });
 
-router.post("/book", (req, res, next) => {
+router.post("/add-book", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
