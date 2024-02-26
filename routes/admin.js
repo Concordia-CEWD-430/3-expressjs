158902b3e87216ac6705ab8bbf6078376e7462d6
PranavBhatia <bhatiapranav1996@gmail.com>
2024-02-26 10:53:56 -0500

    Helper for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/add-book", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-book.html"));
+  res.sendFile(path.join(rootDir, "views", "add-book.html"));
 });
 
 router.post("/add-book", (req, res, next) => {
