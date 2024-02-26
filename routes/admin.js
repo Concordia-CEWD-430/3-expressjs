d8f01fa85edf8a91156d8121cc52eb213acebfd6
PranavBhatia <bhatiapranav1996@gmail.com>
2024-02-26 16:18:50 -0500

    Sharing Data

--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,16 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const books = [];
+
 router.get("/add-book", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-book.html"));
 });
 
 router.post("/add-book", (req, res, next) => {
-  console.log(req.body);
+  books.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.books = books;
