ce9df3e46217d07c2c8f3d86ced412c580e35889
PranavBhatia <bhatiapranav1996@gmail.com>
2024-03-04 12:44:34 -0500

    add 404 controller

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set("view engine", "ejs");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 const rootDir = require("./util/path");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,8 +19,6 @@ app.use(express.static(path.join(rootDir, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
